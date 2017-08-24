9ce34ad755eccfb36ca66495cb0aff4e6ba234ba
Maarten Bicknese <maarten.bicknese@gmail.com>
2017-08-24 06:11:30 +0200

    Load blog posts from contentful

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,11 +1,29 @@
 <template>
   <main>
     <h1>Maaike van Deursen</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.fields.title }}
+      </li>
+    </ul>
   </main>
 </template>
 
 <script>
+import contentful from '../content'
+
 export default {
-  name: 'hello'
+  name: 'hello',
+  data: () => ({
+    posts: []
+  }),
+  mounted () {
+    contentful.getBlogPosts().then(this.setPosts)
+  },
+  methods: {
+    setPosts (posts) {
+      this.posts = posts
+    }
+  }
 }
 </script>
---
+++ src/content/contentful.js
@@ -0,0 +1,14 @@
+import client from './client'
+// import { parseEntries } from 'contentful'
+
+export default {
+  getBlogPosts () {
+    return client
+      .getEntries({ 'content_type': 'blogPost' })
+      .then(response => response.items)
+  },
+  getBlogPost (id) {
+    return client
+      .getEntry(id)
+  }
+}
