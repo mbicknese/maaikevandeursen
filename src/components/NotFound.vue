d832a709947fcb1e5d312e8bec0d726a768b5d63
Maarten Bicknese <maarten.bicknese@gmail.com>
2017-09-01 13:59:24 +0200

    Change to history mode and add 404 page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container text-center">
+    <h1 class="display-2 mt-5 mb-5">
+      <strong>404!</strong><br>
+      <small>pagina bestaat niet!</small>
+    </h1>
+    <p>
+      De pagina die u probeert te bezoeken bestaat niet
+    </p>
+    <p>
+      <a href="/">Ga terug</a> naar de site om verder te gaan
+    </p>
+  </div>
+</template>
+
+<style scoped>
+  h1 strong {
+    color: #6200EA;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Hello from '@/components/Hello'
 import ComingSoon from '@/components/ComingSoon'
-import BlogPost from '@/components/BlogPost'
+// import BlogPost from '@/components/BlogPost'
+import NotFoundComponent from '@/components/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: ComingSoon
     },
-    {
-      path: '/blog/:id',
-      name: 'BlogPost',
-      component: BlogPost,
-      props: true
-    }
+    // {
+    //   path: '/blog/:id',
+    //   name: 'BlogPost',
+    //   component: BlogPost,
+    //   props: true
+    // },
+    { path: '*', component: NotFoundComponent }
   ]
 })
